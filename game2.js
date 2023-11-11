5e5f8f4e02683428ef8e91182824dd0b2f404393
OwenNieuwenhuizen <n.owen@wustl.edu>
2023-11-10 23:51:35 -0600

    game 2 space

--- game2.js
+++ game2.js
@@ -17,7 +17,7 @@ var myGameArea = {
     canvas : document.getElementById("game"),
     
     start : function() {
-        this.canvas.width = 480;
+        this.canvas.width = 780;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
